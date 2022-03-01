6ec173add9ce306f4073113e7c209a97be8b1a32
Blizire <stiles.trenton@gmail.com>
2022-03-01 00:23:20 -0600

    added the functionality to the copy button

--- static/view.js
+++ static/view.js
@@ -1,1 +1,12 @@
-console.log('hello')
+function clipboard(domObject) {
+    domObject.parentNode.parentNode.childNodes.
+        forEach(
+        (x) => {
+            if (x.className == "routes")
+            {
+                console.log(x.href)
+                navigator.clipboard.writeText(x.href);
+            }
+        }
+    );
+} 
